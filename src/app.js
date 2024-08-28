9fd818829bf93762c175d07b1e363d941a9339da
Inayathasan0 <inayathasan82@gmail.com>
2024-08-28 16:08:55 +0530

    user router and controller

--- src/app.js
+++ src/app.js
@@ -20,4 +20,12 @@ app.use(cookieParser());
 
 
 
+// routes 
+import userRouter from './routes/user.routes.js';
+
+// routes declaration
+app.use("/api/v1/users",userRouter);
+
+// https://localhost:8000/api/v1/users/register
+
 export { app }
